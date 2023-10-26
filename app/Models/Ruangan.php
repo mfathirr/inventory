f4c57e18bfd20575fa892a7da66a9a57be3057f1
mfathirr <mulfathir632@gmail.com>
2023-10-26 09:44:32 +0700

    make table, controller, model Ruangan and relation with users table

---
+++ app/Models/Ruangan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Ruangan extends Model
+{
+    use HasFactory;
+    protected $table = 'ruangan';
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsTo('App\User', 'id_user');
+    }
+}
